fe3ec0ecea1d4042143df4339460ea2a43ce81d2
matcho <mathias.chouet@gmail.com>
2016-08-06 23:25:40 +0200

    Système de temporisation par cache (2)

--- munin-floradata/munin-floradata.php
+++ munin-floradata/munin-floradata.php
@@ -49,41 +49,68 @@ if (! empty($indic['intervalle'])) {
 // si un intervalle a été défini, on vérifie la date de dernière écriture du cache
 $recalculer = ($intervalle === false || cacheObsolete($nomIndicateur, $intervalle));
 
-// connexion à la base de données si nécessaire
-$bd = false;
+$valeurs = "";
 if ($recalculer) {
+	// connexion à la base de données
 	$dsn = 'mysql:host=' . $config['bd']['hote'] . ';dbname=' . $config['bd']['base'] . ';port=' . $config['bd']['port'];
 	$bd = new PDO($dsn, $config['bd']['login'], $config['bd']['mdp']);
-}
-
-// boucle sur les séries
-foreach ($indic['series'] as $nomSerie => $serie) {
-	$val = false;
-	if ($recalculer) {	
+	// boucle sur les séries
+	foreach ($indic['series'] as $nomSerie => $serie) {
 		// exécution de la requête
+		// @TODO remplir le cache tout de même en cas d'échec, pour ne pas
+		// réessayer trop souvent
 		$q = $serie["requete"];
 		$res = $bd->query($q);
 		$ligne = $res->fetch();
 		$val = $ligne['val'];
-		// écriture du cache
-	} else {
-		// lecture dans le cache
+		// ajout de la série aux valeurs de l'indicateur
+		$valeurs .= $nomSerie . '.data ' . $val . PHP_EOL;
 	}
-	echo $nomSerie . '.data ' . $val . PHP_EOL;
+	// écriture du cache
+	ecrireCache($nomIndicateur, $valeurs);
+} else {
+	// lecture dans le cache
+	$valeurs = lireCache($nomIndicateur);
 }
+
+// sortie de valeurs
+echo $valeurs;
+
 
 /** retourne true si la date de dernière écriture du fichier de cache pour
 	l'indicateur donné est plus vieille que l'intervalle demandé */
 function cacheObsolete($nomIndicateur, $intervalle) {
 	$fichierCache = calculerNomFichierCache($nomIndicateur);
-	// @TODO lire les stats du fichier
-	return true;
+	$dateModif = 0;
+	if (file_exists($fichierCache)) {
+		$dateModif = filemtime($fichierCache);
+	}
+	$maintenant = time();
+	$diff = $maintenant - $dateModif;
+	//echo "fichier modifié il y a [$diff]\n";
+	$obsolete = ($diff > $intervalle);
+	//echo "obsolete : "; var_dump($obsolete); echo "\n";
+	return $obsolete;
+}
+
+/** écrit dans le cache les valeurs de l'indicateur donné */
+function ecrireCache($nomIndicateur, $valeurs) {
+	$fichierCache = calculerNomFichierCache($nomIndicateur);
+	file_put_contents($fichierCache, $valeurs);
+}
+
+/** lit dans le cache les valeurs de l'indicateur donné */
+function lireCache($nomIndicateur) {
+	$fichierCache = calculerNomFichierCache($nomIndicateur);
+	return file_get_contents($fichierCache);
 }
 
 /** renvoie l'emplacement du fichier de cache correspondant à l'indicateur donné */
 function calculerNomFichierCache($nomIndicateur) {
-	// @TODO
-	return false;
+	global $dossierCache;
+	$fichier = $dossierCache . '/munin-floradata-' . $nomIndicateur . '.cache';
+	//echo "fichier cache: [$fichier]\n";
+	return $fichier;
 }
 
 /** écrit un message d'erreur sur la sortie d'erreur et quitte le programme
